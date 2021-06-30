2cbf972737298f27e951235bd5ceb79d54986f24
siva209 <sivanarayana209@gmail.com>
2021-06-30 06:44:00 +0530

    Added:Added:IF-ELSE program to print number in words

---
+++ selection5.js
@@ -0,0 +1,45 @@
+const prompt = require('prompt-sync')();
+let Num = prompt("Please Number : ");
+if ( Num == 0 )
+{
+console.log("Zero");
+}
+else if ( Num == 1 )
+{
+console.log("One");
+}
+else if ( Num == 2 )
+{
+console.log("two");
+}
+else if ( Num == 3 )
+{
+console.log("Three");
+}
+else if ( Num == 4 )
+{
+console.log("four");
+}
+else if ( Num == 5 )
+{
+console.log("five");
+}
+else if ( Num == 6 )
+{
+console.log("six");
+}
+else if ( Num == 7 )
+{
+console.log("Seven");
+}
+else if ( Num == 8 )
+{
+console.log("eight");
+}
+else if ( Num == 9 )
+{
+console.log("Nine");
+}
+else {
+Console.log("Enter number between 0 to 9");
+}
